7824db8f76fb9a79bcd92534c46fff6ca64055ab
SergioHuesca <sergiohuesca15@gmail.com>
2023-12-05 14:36:43 -0600

    Dither sin animacion

--- ProyectoF/test.m
+++ ProyectoF/test.m
@@ -5,6 +5,7 @@ img = imread("IMG.jpg");
 img_gray = rgb2gray(img);
 a = 0.4; % depende del contraste, la frecuencia espacial, y la orientación de el objetivo
 img_contrast = a * img_gray + (1/2) - (a/2);
+[filas, columnas]=size(img_contrast);
 %división de frecuencias
 
 
@@ -16,3 +17,14 @@ title('Imagen en escala de grises');
 subplot(1,2,2);
 imshow(img_contrast);
 title('Imagen con contraste reducido');
+
+dit = imread("Escudo.png");
+dit = mat2gray(dit);
+dit = imresize(dit,[(filas/5),(columnas/5)]);
+imshow(dit);
+
+dithered=repmat(dit,5);
+
+imshow(dithered);
+
+
