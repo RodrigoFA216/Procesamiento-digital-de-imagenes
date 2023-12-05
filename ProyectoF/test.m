9cf02f815ec60d90d1cd035404fb92d4b20bc017
RodrigoFA216 <rfloresa1900@alumno.ipn.mx>
2023-12-05 13:37:18 -0600

    changes2

--- ProyectoF/test.m
+++ ProyectoF/test.m
@@ -1,4 +1,7 @@
 % Tests
+% añadir las funciones necesarias
+addpath("lptoolbox\");
+
 
 %Carga de una imagen
 img = imread("IMG.jpg");
@@ -6,7 +9,7 @@ img_gray = rgb2gray(img);
 a = 0.4; % depende del contraste, la frecuencia espacial, y la orientación de el objetivo
 img_contrast = a * img_gray + (1/2) - (a/2);
 %división de frecuencias
-
+img_freq = lpdecn(double(img_contrast),'9/7', 7);
 
 
 figure();
