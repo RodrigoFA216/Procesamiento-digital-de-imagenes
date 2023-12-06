598710a9ee323a9234a69198e78cba1022b206e5
RodrigoFA216 <rfloresa1900@alumno.ipn.mx>
2023-12-06 14:48:43 -0600

    cambios finales

--- ProyectoF/ProyectoFinalTeam2.m
+++ ProyectoF/ProyectoFinalTeam2.m
@@ -3,40 +3,54 @@
 % Sergio Huesca Flores
 % Miguel Camargo
 
-%% Inicialización de variables
+% Tests
 
-clear;
-clc;
-opcion = 0;
-% Bucle para mostrar el menú de selección hasta que el usuario elija salir
-while opcion ~= 10
-    % Mostrar el menú
-    %clc
-    disp('Menú de selección:');
-    disp('1. Erosión de una imágen'); %Y
-    disp('2. Filtro inverso con ruido');%Y
-    disp('3. Filtro Wiener');
+%Carga de una imagen
+img = imread("IMG.jpg");
+img_gray = rgb2gray(img);
+img_gray = mat2gray(img_gray);
+a = 0.4; % depende del contraste, la frecuencia espacial, y la orientación de el objetivo
+img_contrast = a * img_gray + (1/2) - (a/2);
+[filas, columnas]=size(img_contrast);
 
-    disp('10. Salir');
-    
-    % Solicitar al usuario que ingrese la opción
-    opcion = input('Ingrese el número de la opción deseada: ');
- 
-    switch opcion
-        case 1
-            disp('Ejecutando Ejercicio 1');
-            ejer1();
-        case 2
-            disp('Ejecutando Ejercicio 2');
-            ejer2();
-        case 3
-            disp('Ejecutando Ejercicio 3');
-            ejer3();
-        case 10
-            disp('Chauchau diria el peso pluma');
-        otherwise
-            disp('Opción no válida. Por favor no sea sapo, elija una opción válida.');
+figure(2);
+hold on;
+subplot(1,2,1);
+imshow(img_gray);
+title('Imagen en escala de grises');
+subplot(1,2,2);
+imshow(img_contrast);
+title('Imagen con contraste reducido');
+hold off;
+
+indice = 1;
+frames = 30;
+figure(3);
+hold on;
+while indice <= frames
+    for y=1:filas
+        for x=1:columnas
+            a = img_contrast(y,x) - 0.8;
+            b = img_contrast(y,x) + 0.8;
+            r = a + (b - a).*rand(1,1)>0.5;
+            In(y,x) = r;
+        end
     end
+    I(indice) = {In};
+    subplot(1,2,1);
+    imshow(In);
+    if indice == 1
+        prom = cell2mat(I(indice));
+        subplot(1,2,2);
+        imshow(prom);
+    else
+        prom = prom + cell2mat(I(indice));
+        subplot(1,2,2);
+        imshow(prom);
+    end
+    subplot(1,2,2);
+    imshow(prom/frames);
+    indice = indice + 1;
 end
 
-disp('¡Gracias por usar el programa!');
+hold off;
